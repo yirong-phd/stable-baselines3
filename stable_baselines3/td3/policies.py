8774522b717beb5ad0fdcbd1f64bc72b3818f21d
Yirong-local <yc118@rice.edu>
2024-06-05 20:57:48 -0500

    updated TD3

--- stable_baselines3/td3/policies.py
+++ stable_baselines3/td3/policies.py
@@ -242,7 +242,7 @@ class TD3Policy(BasePolicy):
         #return self.actor(observation)
         proto_action = self.actor(observation)
         print("proto_action",proto_action)
-        print("action_space: ", self.action_dim)
+        print("action_space: ", self.action_space)
         return proto_action
 
     def set_training_mode(self, mode: bool) -> None:
